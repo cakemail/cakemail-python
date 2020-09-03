2a4e4a765414f269fcf469f1837c101d7484e6f1
Sébastien Grégoire <greg@cakemail.com>
2020-09-03 10:11:45 -0400

    bump 1.0.14

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ requirements = [
 
 setup(
     name='cakemail',
-    version='1.0.12',
+    version='1.0.14',
     description='Cakemail Next-gen API client',
     python_requires='>=3.6',
     long_description=README,
